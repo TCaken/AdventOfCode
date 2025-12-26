c8a16263f14be3594efdb1fbea834740ad702951
Caken Tan <cakentan@gmail.com>
2025-12-26 18:12:46 +0800

    push day 18

---
+++ 2015/Day 18: Like a GIF For Your Yard/first.cpp
@@ -0,0 +1,82 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    vector<vector<bool>> grid;
+    string s;
+
+    while (getline(cin, s)) {
+        vector<bool> tmp;
+        for(auto ch : s){
+            if(ch == '.'){
+                tmp.push_back(false);
+            }
+            else{
+                tmp.push_back(true);
+            }
+        }
+        grid.push_back(tmp);
+    }
+    
+    int n = grid.size();
+    
+    
+    for(int i = 0; i < 100; i++){
+        vector<vector<bool>> tmp(n, vector<bool>(n, false));
+        for(int x = 0; x < n; x++){
+            for(int y = 0; y < n; y++){
+                int sum = 0;
+                if(x > 0){
+                    if(y > 0) sum += grid[x-1][y-1];
+                    sum += grid[x-1][y];
+                    if(y < n-1) sum += grid[x-1][y+1];
+                }
+                
+                if(y > 0) sum += grid[x][y-1];
+                if(y < n-1) sum += grid[x][y+1];
+                
+                if(x < n-1){
+                    if(y > 0) sum += grid[x+1][y-1];
+                    sum += grid[x+1][y];
+                    if(y < n-1) sum += grid[x+1][y+1];
+                }
+                
+                if(grid[x][y]){
+                    if(sum == 2 || sum == 3){
+                        tmp[x][y] = true;
+                    }
+                    else{
+                        tmp[x][y] = false;
+                    }
+                }
+                else{
+                    if(sum == 3){
+                        tmp[x][y] = true;
+                    }
+                    else{
+                        tmp[x][y] = false;
+                    }
+                }
+            }
+        }
+        
+        int ans = 0;
+        grid = tmp;
+        for(int x = 0; x < n; x++){
+            for(int y = 0; y < n; y++){
+                ans += tmp[x][y];
+                // if(tmp[x][y]){
+                //     cout << "#";
+                // }
+                // else{
+                //     cout << ".";
+                // }
+            }
+            // cout << "\n";
+        }
+        cout << i << ": " << ans << "\n";
+    
+    }
+    return 0;
+}
